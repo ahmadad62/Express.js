7220877a740f3d0c95c18de52529eefdd3e97927
ahmadad62 <a.naghian@gmail.com>
2022-12-31 14:19:31 +0100

    24:00

--- routes/users.js
+++ routes/users.js
@@ -11,4 +11,17 @@ router.get('/new', (req, res) => {
     res.send('User new form');
 })
 
+
+router
+    .route("/:id")
+    .get((req, res) => {
+        res.send(`Get User with ID ${req.params.id}`)
+    })
+    .put((req, res) => {
+        res.send(`Update User with ID ${req.params.id}`)
+    })
+    .delete((req, res) => {
+        res.send(`Delete User with ID ${req.params.id}`)
+    })
+
 module.exports = router;
